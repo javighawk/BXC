42f069a61216c2b13d66b149daa5e9c414e9381e
javighawk <jgarci42@hawk.iit.edu>
2016-08-07 13:08:11 -0600

    Changed comments format

--- src/Bercianity_Server/src/Telemetry.java
+++ src/Bercianity_Server/src/Telemetry.java
@@ -4,49 +4,34 @@ import java.util.concurrent.Semaphore;
 
 public class Telemetry{
 	
+	/* Telemetry identifiers */
 	public final static byte TEL_SPEED  	  = 0x00;
 	public final static int  TEL_TIMES		  = 0x17;
 	public final static int  TEL_TIMELABELS   = 0x18;
 	public final static byte ENDOFTM		  = 0x05;
 	
-	/*
-	 * Time data
-	 */
+	/* Time data */
 	private static long[] times;
 	
-	/*
-	 * Time data labels
-	 */
+	/* Time data labels */
 	private static String []labels;
 
-	/*
-	 * Motors speed data
-	 */
+	/* Motors speed data */
 	private static short[] mSpeed;
 
-	/*
-	 * Determines whether we are recording data
-	 */
+	/* Determines whether we are recording data */
 	private static boolean isRecordingData = false;
 
-	/*
-	 * File descriptors
-	 */
+	/* File descriptors */
 	private static File timeData, mSpeedData;
 	
-	/*
-	 * File writers
-	 */
+	/* File writers */
 	private static FileWriter timeWriter, mSpeedWriter;
 	
-	/*
-	 * Buffered writers
-	 */
+	/* Buffered writers */
 	private static BufferedWriter timeBW, mSpeedBW;
 	
-	/*
-	 * Semaphore to control the Files access
-	 */
+	/* Semaphore to control the Files access */
 	private static Semaphore filesSemaphore = new Semaphore(1);
 	
 	/*
@@ -56,17 +41,17 @@ public class Telemetry{
 		
 		int inputTelemetry;
 		
-		/* Keep reading until END OF TM flag is received */
+		// Keep reading until END OF TM flag is received
 		while( (inputTelemetry = MainAction.arduino.readData()) != ( Telemetry.ENDOFTM )){
 			
-			/* Record the number of records and size of each for the incoming data */
+			// Record the number of records and size of each for the incoming data
 			int nRecs, sizeRecs;
 			while((nRecs = MainAction.arduino.readData()) == -1);
 			while((sizeRecs = MainAction.arduino.readData()) == -1);
 
 			switch(inputTelemetry){
 			
-				/* Time records */
+				// Time records
 				case TEL_TIMES:
 					times = new long[nRecs];
 					for( int i=0 ; i<nRecs ; i++ )
@@ -77,7 +62,7 @@ public class Telemetry{
 					MainAction.GUI.setTimeBarValues(times);
 					break;	
 					
-				/* Time record labels */
+				// Time record labels
 				case TEL_TIMELABELS:
 					labels = new String[nRecs];
 					for( int i=0 ; i<nRecs ; i++ ){
@@ -91,7 +76,7 @@ public class Telemetry{
 					MainAction.GUI.setTimeLabels(labels);
 					break;	
 				
-				/* Speed information */
+				// Speed information
 				case TEL_SPEED:
 					mSpeed = new short[nRecs];
 					for( int i=0 ; i<nRecs ; i++ )
@@ -102,7 +87,7 @@ public class Telemetry{
 					MainAction.GUI.setSpeeds(mSpeed[0], mSpeed[1]);					
 					break;
 
-				/* Unidentified Telemetry data */
+				// Unidentified Telemetry data
 				default:
 					MainAction.GUI.println("[Server]: Unknown telemetry ID: 0x" + Integer.toHexString(inputTelemetry));
 					for( int i=0 ; i<nRecs ; i++ )
@@ -113,7 +98,7 @@ public class Telemetry{
 			}	
 		}
 		
-		/* Record data */
+		// Record data
 		if( isRecordingData )
 			recordCurrentData();
 	}
@@ -125,29 +110,29 @@ public class Telemetry{
 		
 		try{
 			
-			/* Request access */
+			// Request access
 			filesSemaphore.acquire();
 		
-			/* Initialize files */
+			// Initialize files
 			timeData = new File("timeData.txt");
 			mSpeedData = new File("mSpeedData.txt");
 			
-			/* Create files (overwrite if they exist) and folder */
+			// Create files (overwrite if they exist) and folder
 			timeData.createNewFile();
 			mSpeedData.createNewFile();
 			
-			/* Initialize FileWriters */
+			// Initialize FileWriters
 			timeWriter = new FileWriter(timeData.getAbsoluteFile());
 			mSpeedWriter = new FileWriter(mSpeedData.getAbsoluteFile());
 			
-			/* Initialize Buffered writers */
+			// Initialize Buffered writers
 			timeBW = new BufferedWriter(timeWriter);
 			mSpeedBW = new BufferedWriter(mSpeedWriter);
 			
-			/* Turn flag up */
+			// Turn flag up
 			isRecordingData = true;
 
-			/* Release access */
+			// Release access
 			filesSemaphore.release();
 		
 		} catch (Exception e){
@@ -162,25 +147,25 @@ public class Telemetry{
 		
 		try{
 			
-			/* Request access */
+			// Request access
 			filesSemaphore.acquire();
 			
 			if( isRecordingData ){
 			
-				/* Write time data */
+				// Write time data
 				timeBW.write(Long.toString(times[0]) + "\t");
 				timeBW.write(labels[0] + ":" + Long.toString(times[1]) + "\t");
 				timeBW.write(labels[1] + ":" + Long.toString(times[2]) + "\t");
 				timeBW.write(labels[2] + ":" + Long.toString(times[3]));
 				timeBW.newLine();
 				
-				/* Write motors speed data */
+				// Write motors speed data
 				mSpeedBW.write(Integer.toString(mSpeed[0]) + "\t");
 				mSpeedBW.write(Integer.toString(mSpeed[1]));
 				mSpeedBW.newLine();				
 			}
 			
-			/* Release access */
+			// Release access
 			filesSemaphore.release();
 			
 		} catch (Exception e){
@@ -195,17 +180,17 @@ public class Telemetry{
 		
 		try {
 			
-			/* Request access */
+			// Request access
 			filesSemaphore.acquire();
 			
-			/* Close buffered writers */
+			// Close buffered writers
 			timeBW.close();
 			mSpeedBW.close();
 			
-			/* Turn flag down */
+			// Turn flag down
 			isRecordingData = false;
 			
-			/* Release access */
+			// Release access
 			filesSemaphore.release();
 			
 		} catch (Exception e) {
