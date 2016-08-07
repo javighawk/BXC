b41d814575042a41f9d48993690782ce2f354f54
javighawk <jgarci42@hawk.iit.edu>
2016-08-07 13:21:55 -0600

    Changed comments format and spacing between functions

--- src/Bercianity_Server/src/MainAction.java
+++ src/Bercianity_Server/src/MainAction.java
@@ -1,31 +1,41 @@
 import java.io.IOException;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
 import javax.swing.JOptionPane;
 
 public class MainAction{
-
+	
+	/* Threads */
 	public static InputCommThread inputS;
 	public static OutputCommThread outputS;
 	public static TimeOutThread timeOut;
 	
+	/* Graphic interface */
 	public static GraphicInterface GUI;
+	
+	/* COMM */
 	public static Comm arduino;
 	
+	/* Locks */
 	public static final Lock InputCOMMLock = new ReentrantLock();
 	public static final Lock OutputCOMMLock = new ReentrantLock();
 
 	
+	/*
+	 * Main method
+	 * 
+	 * @param args Arguments
+	 */
 	public static void main(String[] args){
 		
-		/* Initialize GUI */
+		// Initialize GUI */
 		GUI = new GraphicInterface();
 		GUI.setInitial();
 		
-		/* Initialize COMM */
+		// Initialize COMM */
 		arduino = new Comm();
 	}
+	
 	
 	/*
 	 * Show error on GUI
@@ -37,47 +47,67 @@ public class MainAction{
                 JOptionPane.ERROR_MESSAGE);
     }
 	
+	
+	/*
+	 * Show error due to Input IOException
+	 * 
+	 * @param e The IOException
+	 */
 	public static void showInputIOException(IOException e){
 		MainAction.GUI.println("[Server]: IOException caught on Input stream. Pausing system...");
 		pauseSystem();
     }
 	
+	
+	/*
+	 * Show error due to Output IOException
+	 * 
+	 * @param e The IOException
+	 */
 	public static void showOutputIOException(IOException e){
 		MainAction.GUI.println("[Server]: IOException caught on Output stream. Pausing system...");
 		pauseSystem();
     }
 	
+	
+	/*
+	 * Connect to Bercianity
+	 */
 	public static void connect(){
 		
-		/* Wait for connection */
+		// Wait for connection */
 		GUI.println("-----Connecting-----");
 		arduino.initialize();
 		 
-		/* Set GUI */
+		// Set GUI */
 		GUI.setConnected();
 		GUI.addKeyListener(new ArduinoKeyListener());
 		
-		/* Initialize threads */
+		// Initialize threads */
 		inputS = new InputCommThread();
 		outputS = new OutputCommThread();
 		timeOut = new TimeOutThread();
 		
-		/* Start threads */
+		// Start threads */
 		inputS.start();
 		outputS.start();
 		timeOut.start();
 	}	
 	
+	
+	/*
+	 * Pause the system. Execute when connection is lost
+	 */
 	public static void pauseSystem(){
 		
-		/* Pause threads */
+		// Pause threads */
 		inputS.pause();
 		timeOut.pause();
 		
-		/* Close COMM */
+		// Close COMM */
 		arduino.closeSockets();
 		
-		/* Set initial window */
+		// Set initial window */
 		GUI.setInitial();
 	}
 }
