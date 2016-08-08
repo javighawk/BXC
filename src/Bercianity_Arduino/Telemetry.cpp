8204a09c565b4964c2d3eca883f98f61cf8ecbaf
javighawk <jgarci42@hawk.iit.edu>
2016-08-07 20:42:24 -0600

    Rearranged telemetry code

--- src/Bercianity_Arduino/Telemetry.cpp
+++ src/Bercianity_Arduino/Telemetry.cpp
@@ -1,5 +1,10 @@
 #include "Telemetry.h"
 
+/* Extern declarations */
+int MAIN_getNAllTimeRecord();
+TimeRecord **MAIN_getAllTimeRecord();
+extern TimeRecord tTelemetry;
+
 /* Auxiliar variable to measure Telemetry period */
 unsigned long timer;
 
@@ -9,14 +14,91 @@ boolean pendingTM = false;
 /* Other TM Pending Boolean Variables */
 boolean pendingMotorSpeedTM = true;
 boolean pendingTimeLabelsTM = true;
+    
 
-/* Extern variables */
-extern TimeRecord tLoop;
-extern TimeRecord tInfoRead;
-extern TimeRecord tTelemetry;
-extern TimeRecord tInfoRead;
-extern TimeRecord tAvailable;
-extern TimeRecord tConnected;
+/*
+ * Send motors user speed
+ */
+void TM_mSpeedTelemetry(){
+    
+    // Motors speed
+    int mSpeed[2] = {MVM_getMotorSpeed(MOTOR_L),
+                     MVM_getMotorSpeed(MOTOR_R)};
+
+    // hEADER
+    uint8_t header[3] = {sizeof(mSpeed[0]), sizeof(mSpeed)/sizeof(mSpeed[0]), TEL_SPEED};
+    
+    // Send Motors speed
+    COMM_write( header, 3 );
+    for( int i=0 ; i<header[1] ; i++ )
+        COMM_write( (uint8_t*)&mSpeed[i], header[0] );
+}
+
+
+/*
+ * Send time telemetry
+ */
+void TM_timeTelemetry(){
+    // Get array of all TimeRecord pointers and how many
+    TimeRecord **allTR = MAIN_getAllTimeRecord();
+    int nAllTR = MAIN_getNAllTimeRecord();
+
+    // Get current time
+    unsigned long nowTime = millis();
+
+    // Get average time of all TimeRecord objects
+    unsigned long avgTimeRecord[nAllTR];
+    for( int i=0 ; i<nAllTR ; i++ ){
+        avgTimeRecord[i] = allTR[i]->getAvgTime();
+
+        // Reset all maxTime for TimeRecord objects
+        allTR[i]->resetMaxTRec();
+    }
+
+    // Prepare Header
+    uint8_t header[3] = {sizeof(nowTime), (uint8_t)(nAllTR + 1), TEL_TIMES};
+    
+    // Send first the current time, and then all the TimeRecord average times
+    COMM_write( header, 3 );
+    COMM_write( (uint8_t*)&nowTime, header[0] );
+    for( int i=0 ; i<nAllTR ; i++ )
+        COMM_write( (uint8_t*)&avgTimeRecord[i], header[0] );
+}
+
+
+/*
+ * Send the time records labels
+ */
+void TM_timeLabelsTelemetry(){
+    // Get array of all TimeRecord pointers and how many
+    TimeRecord **allTR = MAIN_getAllTimeRecord();
+    int nAllTR = MAIN_getNAllTimeRecord();
+    
+    // Initialize variables to extract the TimeRecord labels
+    char labels[nAllTR][LABEL_LENGTH];
+    String labels_str[nAllTR];
+
+    // Iterate through all TimeRecord objects
+    for( int i=0 ; i<nAllTR ; i++ ){
+        // Get label
+        labels_str[i] = allTR[i]->getLabel();
+
+        // Copy the character array into a String. This will force all
+        // labels to have the same length
+        for( int j=0 ; j<LABEL_LENGTH ; j++ ){
+            if( j < labels_str[i].length() ) labels[i][j] = labels_str[i][j];
+            else labels[i][j] = 0;
+        }
+    }
+
+    // Prepare header to send
+    uint8_t header[3] = {sizeof(labels[0]), sizeof(labels)/sizeof(labels[0]), TEL_TIMELABELS};
+    COMM_write( header, 3 );
+
+    // Send all labels
+    for( int i=0 ; i<nAllTR ; i++ )
+        COMM_write( (uint8_t*)labels[i], header[0] );
+}
 
 
 /*
@@ -27,81 +109,29 @@ void TM_end(){
     COMM_flush();
     pendingTM = false;
 }
-    
 
-void TM_motorsSpeedTelemetry(){
-    
-    /* Motors speed */
-    int mSpeed[2] = {MVM_getMotorSpeed(1),
-                     MVM_getMotorSpeed(2)};
-    uint8_t header[3] = {sizeof(mSpeed[0]), sizeof(mSpeed)/sizeof(mSpeed[0]), TEL_SPEED};
-    
-    // Send Motors speed
-    COMM_write( header, 3 );
-    for( int i=0 ; i<header[1] ; i++ )
-        COMM_write( (uint8_t*)&mSpeed[i], header[0] );
-}
-  
-  
-void TM_timeTelemetry(){
 
-    unsigned long times[6] = {millis(),
-                              tLoop.TIME_getAvgTime(),
-                              tTelemetry.TIME_getAvgTime(),
-                              tConnected.TIME_getAvgTime(),
-                              tAvailable.TIME_getAvgTime(),
-                              tInfoRead.TIME_getAvgTime()};
-    
-    uint8_t header[3] = {sizeof(times[0]), sizeof(times)/sizeof(times[0]), TEL_TIMES};
-    COMM_write( header, 3 );
-    COMM_write( (uint8_t*)&times[0], header[0] );
-    COMM_write( (uint8_t*)&times[1], header[0] );
-    COMM_write( (uint8_t*)&times[2], header[0] );
-    COMM_write( (uint8_t*)&times[3], header[0] );
-    COMM_write( (uint8_t*)&times[4], header[0] );
-    COMM_write( (uint8_t*)&times[5], header[0] );
-
-    // Reset the records
-    tLoop.TIME_resetMaxTRec();
-    tInfoRead.TIME_resetMaxTRec();
-    tTelemetry.TIME_resetMaxTRec();
-}
-
-void TM_timeLabelsTelemetry(){
-  
-    char labels[5][LABEL_LENGTH];
-    String labels_str[5] = {tLoop.TIME_getLabel(),
-                            tTelemetry.TIME_getLabel(),
-                            tConnected.TIME_getLabel(),
-                            tAvailable.TIME_getLabel(),
-                            tInfoRead.TIME_getLabel()};
-
-    for( int i=0 ; i<LABEL_LENGTH ; i++ )
-        for( int j=0 ; j<5 ; j++ )
-            if( i < labels_str[0].length() ) labels[j][i] = labels_str[j][i];
-            else labels[j][i] = 0;
-
-    uint8_t header[3] = {sizeof(labels[0]), sizeof(labels)/sizeof(labels[0]), TEL_TIMELABELS};
-    COMM_write( header, 3 );
-    COMM_write( (uint8_t*)labels[0], header[0] );
-    COMM_write( (uint8_t*)labels[1], header[0] );
-    COMM_write( (uint8_t*)labels[2], header[0] );
-    COMM_write( (uint8_t*)labels[3], header[0] );
-    COMM_write( (uint8_t*)labels[4], header[0] );
-  
-}
-
+/*
+ * Start sending telemetry
+ */
 void TM_start(){  
-
     if( pendingTM ){
+        // Record Telemetry time
+        tTelemetry.trigger();
+
+        // Send TM identifier
         COMM_write( TELEMETRY );
-        
+
+        // Send time telemetry
         TM_timeTelemetry();
-        
+
+        // Check pending motor speed telemetry
         if( pendingMotorSpeedTM ){
-            TM_motorsSpeedTelemetry();
+            TM_mSpeedTelemetry();
             pendingMotorSpeedTM = false;
         }
+
+        // Check pending time labels TM
         if( pendingTimeLabelsTM ){
             TM_timeLabelsTelemetry();
             pendingTimeLabelsTM = false;
@@ -110,9 +140,13 @@ void TM_start(){
         TM_end();      
     }
 }
-  
+
+
+/*
+ * Check if a telemetry petition needs to be sent
+ */  
 void TM_checkTelemetry(){
-  
+    // Send TM petition each TELEMETRYTIME period
     if( (!pendingTM) && ((millis() - timer) >= TELEMETRYTIME) ){
         timer = millis();
         COMM_write( TM_PETITION );
@@ -121,6 +155,17 @@ void TM_checkTelemetry(){
     }    
 }
 
+
+/*
+ * Cancel pending telemetry
+ */
+void TM_cancel(){
+    pendingTM = false;
+}
+
+
+/*
+ * Pending telemetry functions
+ */
 void TM_pendMotorSpeed(){ pendingMotorSpeedTM = true; }
 void TM_pendTimeLabels(){ pendingMotorSpeedTM = true; }
-void TM_cancel(){ pendingTM = false; }
--- src/Bercianity_Arduino/Telemetry.h
+++ src/Bercianity_Arduino/Telemetry.h
@@ -16,27 +16,10 @@
 /**************** DEFINES ****************/
 /*****************************************/
 
-#define TELEMETRY         0x0E
-#define TM_PETITION       0x0F
 #define TEL_SPEED         0x00
-#define TEL_BYTEINFO      0x02
-#define TEL_TIMEOUT       0x03
-#define TEL_ACCEL         0x06
-#define TEL_DIFFMOTOR     0x09
-#define TEL_PIDVALUES     0x0A
-#define TEL_ALPHA_ACCEL   0x0B
-#define TEL_ACCELOFFSETS  0x0C
-#define TEL_GYRO          0x0D
-#define TEL_ALPHA_GYRO    0x10
-#define TEL_ALPHA_DEG     0x11
-#define TEL_GYROOFFSETS   0x12
-#define TEL_MOTORSPOWER   0x13
-#define TEL_MOTOROFFSETS  0x14
 #define TEL_TIMES         0x17
 #define TEL_TIMELABELS    0x18
-#define ENDOFTM           0x05
-
-#define TELEMETRYTIME       40
+#define TELEMETRYTIME       20
 
 
 /*****************************************/
